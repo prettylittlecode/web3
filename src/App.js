74f42c573a49146b7c6299d14a7d5bae0ea5f777
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2023-12-30 14:01:53 +1200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import TypingText from './TypingText';
+import WalletConnect from './WalletConnect';
+
+function App() {
+  return (
+    <div className="App">
+      <TypingText/>
+      <WalletConnect/>
+    </div>
+  );
+}
+
+export default App;
